15cd226243d44764af5d7db2dd045057d4a052c5
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2024-02-23 14:38:58 +0100

    add create, update and delete functionality to invoice page

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -18,7 +18,6 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
-;
   }
 
   return (
